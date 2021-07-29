24ebde39572d8297f728000d22b60f042605763e
Anurag Kumar <anuragsingh4512@gmail.com>
2021-07-29 22:44:51 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-var username= prompt("Enter your username")
-alert("Hello "+username+
-" are u robot?")
+var btnTranslate = document.querySelector("#btn-translate") 
+var txtInput = document.querySelector("#txt-input")
+var divOutput = document.querySelector("#output")
+
+console.log(btnTranslate)
+console.log(txtInput)
+console.log(divOutput)
+
+
+const text1 = "I am ShaktiMan"
+const text2 = "I am IronMan"
+const text3 = "I am superman"
+
+function constructUrl(text){
+    return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + "?" + text 
+}
+
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked")
+    console.log("input", txtInput.value)
+    divOutput.innerText = "Anurag kumar " + txtInput.value
+
+})
